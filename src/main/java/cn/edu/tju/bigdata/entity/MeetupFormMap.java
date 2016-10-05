d051f40e84ed14b73eea02f4294648d35778b533
eggiter <haodonglyu@gmail.com>
2016-10-05 11:43:26 +0800

    修改：生物黑客组织数据集的增删改查（即第一个闭环：上传-审核-查询）。

--- src/main/java/cn/edu/tju/bigdata/entity/MeetupFormMap.java
+++ src/main/java/cn/edu/tju/bigdata/entity/MeetupFormMap.java
@@ -6,6 +6,6 @@ import cn.edu.tju.bigdata.util.FormMap;
 /**
  * Created by Administrator on 2016/9/28.
  */
-@TableSeg(tableName = "meetup", id = "id")
+@TableSeg(tableName = "bd_meetup", id = "id")
 public class MeetupFormMap extends FormMap<String, Object> {
 }
